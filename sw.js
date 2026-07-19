70138e02395c6945d18297e62d7a47cb62a39457
Umer Waseem <itzbloom90@gmail.com>
2026-07-20 02:07:43 +0500

    Updates

---
+++ sw.js
@@ -0,0 +1,20 @@
+// Service Worker for Pulse App 
+const CACHE_NAME = 'pulse-v1'; 
+const urlsToCache = [ 
+  '/pulse7/', 
+  '/pulse7/index.html', 
+  '/pulse7/manifest.json' 
+]; 
+ 
+self.addEventListener('install', event =
+  event.waitUntil( 
+    caches.open(CACHE_NAME) 
+      .then(cache =
+  ); 
+}); 
+ 
+self.addEventListener('fetch', event =
+  event.respondWith( 
+    caches.match(event.request) 
+  ); 
+}); 
